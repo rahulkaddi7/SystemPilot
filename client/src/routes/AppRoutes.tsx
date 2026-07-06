650ce5cff382b7f6b9eb965173d7c2de44b957f3
Rahul Kaddi <rahulkaddi@ncompass.inc>
2026-07-07 01:15:26 +0530

    refactor: folder structure

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+import Landing from "../pages/Landing/Landing";
+import Chat from "../pages/Chat/Chat";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  );
+}
